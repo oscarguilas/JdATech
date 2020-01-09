d03afebfa24a2ef572b1cc7ac2bebe06749e5b70
oscarguilas <cf19oscar.guillen@iesjoandaustria.org>
2020-01-09 19:44:02 +0100

    WIP Working on date display

--- src/main/java/com/example/jdatech/ui/home/HomeFragment.java
+++ src/main/java/com/example/jdatech/ui/home/HomeFragment.java
@@ -1,13 +1,17 @@
 package com.example.jdatech.ui.home;
 
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -30,6 +34,41 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //Get the display
+        WindowManager wm = (WindowManager) getContext().getSystemService(getContext().WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+
+        //Getting the display size
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
+        int smallSide;
+        //Finding the small side of the screen
+        if(width > height) smallSide = height;
+        else if(height > width) smallSide = width;
+        else smallSide = width;
+
+        Double convSmall = smallSide*0.35;
+        smallSide =(int) Math.round(convSmall);
+
+        ConstraintLayout dateBackground = root.findViewById(R.id.dateLayout);
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) dateBackground.getLayoutParams();
+
+        params.height = smallSide;
+        params.width = smallSide;
+
+        dateBackground.setLayoutParams(params);
+
+        TextView dateText = root.findViewById(R.id.dateText);
+
+        //TODO: Format date text to better fit the shape and generally look better
+
         return root;
+
+
+
+
     }
 }
