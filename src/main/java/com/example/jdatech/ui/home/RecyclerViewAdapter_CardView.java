fdcf12b26b98d1a283e93ac80072f43b40e61a9f
oscarguilas <cf19oscar.guillen@iesjoandaustria.org>
2020-01-28 19:56:33 +0100

    (WIP) Confused with CardView

---
+++ src/main/java/com/example/jdatech/ui/home/RecyclerViewAdapter_CardView.java
@@ -0,0 +1,64 @@
+package com.example.jdatech.ui.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.jdatech.R;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter_CardView extends RecyclerView.Adapter<RecyclerViewAdapter_CardView.MyViewHolder> {
+    private ArrayList<String> dataset;
+    private LayoutInflater mInflater;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public MyViewHolder(View v) {
+            super(v);
+            textView = v.findViewById(R.id.tvRecycler);
+
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public RecyclerViewAdapter_CardView(Context context, ArrayList<String> list) {
+        dataset = list;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public RecyclerViewAdapter_CardView.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                 int viewType) {
+
+        // create a new view
+        View view = mInflater.inflate(R.layout.horario_textview, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(view);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        //String content = dataset[position];
+        //holder.textView.setText(content);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return dataset.size();
+    }
+}
